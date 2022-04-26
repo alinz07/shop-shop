e0b3e7ba7b094876147d2d50eb68984eb49e6bd1
Tony Linz <tonyllinz@gmail.com>
2022-04-26 17:55:22 -0500

    replaced global state variable props in the components that use them

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,50 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-
-import { QUERY_PRODUCTS } from '../utils/queries';
-import spinner from '../assets/spinner.gif';
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { useStoreContext } from "../utils/GlobalState";
+import { UPDATE_PRODUCTS } from "../utils/actions";
+import { QUERY_PRODUCTS } from "../utils/queries";
+import spinner from "../assets/spinner.gif";
 
 function Detail() {
-  const { id } = useParams();
+    const [state, dispatch] = useStoreContext();
 
-  const [currentProduct, setCurrentProduct] = useState({});
+    const { id } = useParams();
 
-  const { loading, data } = useQuery(QUERY_PRODUCTS);
+    const [currentProduct, setCurrentProduct] = useState({});
 
-  const products = data?.products || [];
+    const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  useEffect(() => {
-    if (products.length) {
-      setCurrentProduct(products.find((product) => product._id === id));
-    }
-  }, [products, id]);
+    const { products } = state;
 
-  return (
-    <>
-      {currentProduct ? (
-        <div className="container my-1">
-          <Link to="/">← Back to Products</Link>
+    useEffect(() => {
+        if (products.length) {
+            setCurrentProduct(products.find((product) => product._id === id));
+        } else if (data) {
+            dispatch({
+                type: UPDATE_PRODUCTS,
+                products: data.products,
+            });
+        }
+    }, [products, data, dispatch, id]);
 
-          <h2>{currentProduct.name}</h2>
+    return (
+        <>
+            {currentProduct ? (
+                <div className="container my-1">
+                    <Link to="/">← Back to Products</Link>
 
-          <p>{currentProduct.description}</p>
+                    <h2>{currentProduct.name}</h2>
 
-          <p>
-            <strong>Price:</strong>${currentProduct.price}{' '}
-            <button>Add to Cart</button>
-            <button>Remove from Cart</button>
-          </p>
+                    <p>{currentProduct.description}</p>
 
-          <img
-            src={`/images/${currentProduct.image}`}
-            alt={currentProduct.name}
-          />
-        </div>
-      ) : null}
-      {loading ? <img src={spinner} alt="loading" /> : null}
-    </>
-  );
+                    <p>
+                        <strong>Price:</strong>${currentProduct.price}{" "}
+                        <button>Add to Cart</button>
+                        <button>Remove from Cart</button>
+                    </p>
+
+                    <img
+                        src={`/images/${currentProduct.image}`}
+                        alt={currentProduct.name}
+                    />
+                </div>
+            ) : null}
+            {loading ? <img src={spinner} alt="loading" /> : null}
+        </>
+    );
 }
 
 export default Detail;
